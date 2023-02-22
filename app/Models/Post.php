a8ad4ce7dbcaf5a471a6372d8edd6c53091f58b2
Фатаев Фарход <FFataev@kpfu.ru>
2023-02-22 09:37:03 +0300

    create app & mgration & bootstrap

---
+++ app/Models/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $table = 'posts';
+
+    //разрешаем изменять данные в таблице
+    protected $guarded = false;
+}
